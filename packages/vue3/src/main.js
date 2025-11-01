8a2226171db7eadb5ef5b1dd57ce32a39f752181
MohammadShadid <mohammadshadid03@gmail.com>
2025-11-01 21:52:59 +0200

    feat(vue3): export ExtendedDataType for external use
    - Export ExtendedDataType from main.js for public API access
    - Maintain compatibility with existing DataType from common package
    - Enable users to reference DATE type in filteringOptions

--- packages/vue3/src/main.js
+++ packages/vue3/src/main.js
@@ -7,6 +7,9 @@ export * from "./utils.js"
 // Export types from common package for convenience
 export { FilterType, GroupType, DataType } from "@visual-filter/common"
 
+// Export our extended DataType that includes DATE support
+export { ExtendedDataType } from "./utils.js"
+
 VueVisualFilter.install = (app) => {
   app.component(VueVisualFilter.name, VueVisualFilter)
 }
